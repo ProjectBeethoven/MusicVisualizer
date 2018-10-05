1950ca1385dfb2590ce0faeb00d9c8aeb45c6313
huynhducmanh <32647896+huynhducmanh@users.noreply.github.com>
2018-10-05 08:51:07 -0400

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -5,6 +5,6 @@ IsaacMick-patch-1
 	std::cout << "Hello, The Bestest Team Ever!!!" << std::endl;
 	int i = 0;
 	while(i < 5){
-		std::cout << "There are" << i << "members in the bestest team in the whole wide world!" << std::endl;
+		std::cout << "There are" << i << "members in the bestest team in the whole wide world!!!!" << std::endl;
 		i++;
 }
