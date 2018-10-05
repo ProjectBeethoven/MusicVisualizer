a693e84999fe3256bc4011282a35c5cd3b0c3cf5
huynhducmanh <32647896+huynhducmanh@users.noreply.github.com>
2018-10-05 08:40:22 -0400

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
 int main(){
-	std::cout << "Hello, The Bestest Team Ever" << std::endl; //Added comma for grammar purposes
+	std::cout << "Hello, The Bestest Team Ever!" << std::endl; //Added comma for grammar purposes
 }
