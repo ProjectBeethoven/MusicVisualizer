22b2a98a7b9d0a8dacdde0fd448042a12235cd6e
SotcAGK <32642025+SotcAGK@users.noreply.github.com>
2018-10-05 08:48:33 -0400

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -3,4 +3,8 @@
 int main(){
 IsaacMick-patch-1
 	std::cout << "Hello, The Bestest Team Ever!!!" << std::endl;
+	int i = 0;
+	while(i < 5){
+		std::cout << "There are" << i << "members in the bestest team in the whole wide world!" << std::endl;
+		i++;
 }
